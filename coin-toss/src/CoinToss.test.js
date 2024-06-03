c82ddd916bb30e3ed05f33a0c72102f7545547f6
Jomkit <j.jujaroen96@gmail.com>
2024-06-02 23:06:53 -0400

    added tests to make sure coin toss counter was incrementing properly

--- coin-toss/src/CoinToss.test.js
+++ coin-toss/src/CoinToss.test.js
@@ -23,3 +23,32 @@ it("should render a coin image after pressing flip button", () => {
     fireEvent.click(flipBtn);
     expect(container.querySelector("img")).toBeInTheDocument();
 })
+
+it("should increment heads", () => {
+    jest.spyOn(Math, "random")
+        .mockReturnValueOnce(1.0)
+        .mockReturnValueOnce(1.0);
+    
+    const { container } = render(<CoinToss />);
+    const flipBtn = container.querySelector("button");
+    fireEvent.click(flipBtn);
+    expect(container.querySelector('p')).toContainHTML("Out of 1 flips, there have been 1 heads and 0 tails");
+    fireEvent.click(flipBtn);
+    expect(container.querySelector('p')).toContainHTML("Out of 2 flips, there have been 2 heads and 0 tails");
+})
+
+it("should increment tails", () => {
+    jest.spyOn(Math, "random")
+        .mockReturnValueOnce(0.5)
+        .mockReturnValueOnce(0.5)
+        .mockReturnValueOnce(1.0);
+    
+    const { container } = render(<CoinToss />);
+    const flipBtn = container.querySelector("button");
+    fireEvent.click(flipBtn);
+    expect(container.querySelector('p')).toContainHTML("Out of 1 flips, there have been 0 heads and 1 tails");
+    fireEvent.click(flipBtn);
+    expect(container.querySelector('p')).toContainHTML("Out of 2 flips, there have been 0 heads and 2 tails");
+    fireEvent.click(flipBtn);
+    expect(container.querySelector('p')).toContainHTML("Out of 3 flips, there have been 1 heads and 2 tails");
+})
